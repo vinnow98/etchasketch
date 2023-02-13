dfafca4b1193eb928783a6d1d25c6f79e4a15f4e
Evin <evinwoon@gmail.com>
2023-02-13 10:20:31 +0800

    fixed bug where grid turns on when sizechanged even though grid option is off

--- index.js
+++ index.js
@@ -1,133 +1,147 @@
 colour = document.getElementById("colourPicker").value;
  colourPicker = document.querySelector("#colourPicker");
+ 
 
- mouseDown = false;
- border = true;
- random = false;
- erasor = false;
- document.body.onmousedown = () => {
-   mouseDown = true;
- };
- document.body.onmouseup = () => {
    mouseDown = false;
- };
- function populateBoard(size) {
-   let board = document.querySelector(".board");
-   let squares = board.querySelectorAll("pixels");
-   squares.forEach((pixels) => pixels.remove());
-   board.style.display = "grid";
-   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-   amount = size * size;
-   for (i = 0; i < amount; i++) {
-     let square = document.createElement("pixels");
-     square.style.backgroundColor = "white";
-     square.style.border = "0.2px solid black";
-     square.addEventListener("mouseover", squareEvent);
-     square.addEventListener("mousedown", squareEvent);
-     board.appendChild(square);
-   }
-   let sizeInput = document.querySelector(".size");
-   sizeInput.textContent = `${size} x ${size}`;
- }
-
- function squareEvent(e) {
-   if (e.type === "mouseover" && !mouseDown) return;
-   if (colour == "random") {
-     randomColour = Math.floor(Math.random() * 16777215).toString(16);
-     this.style.backgroundColor = "#" + randomColour;
-   } else {
-     this.style.backgroundColor = colour;
-   }
- }
-
- //start randomButtom
- function toggleRandom() {
-   if (!random) {
-     randomOn();
-     colourPickerOff();
-     erasorOff();
-   } else {
-     randomOff();
-     colourPickerOn();
-     //colour changes to current colourpicker colour
-     colour = colourPicker.value;
-   }
- }
- function randomOn() {
-   randomButton.style.backgroundColor = "grey";
-   randomButton.style.transform = "translate(2px, 2px)";
-   random = true;
- }
-
- function randomOff() {
-   randomButton.style.backgroundColor = "lightgray";
-   randomButton.style.transform = "none";
+   border = true;
    random = false;
- }
- // end of randomButton
-
- //start erasorButton()
- function toggleErasor() {
-   if (!erasor) {
-     erasorOn();
-     randomOff();
-     colourPickerOff();
-   } else {
-     erasorOff();
-     colourPickerOn();
-     colour = colourPicker.value;
-   }
- }
-
- function erasorOn() {
-   erasorButton.style.backgroundColor = "grey";
-   erasorButton.style.transform = "translate(2px, 2px)";
-   erasor = true;
- }
-
- function erasorOff() {
-   erasorButton.style.backgroundColor = "lightgray";
-   erasorButton.style.transform = "none";
    erasor = false;
- }
- //end erasorButton
+   document.body.onmousedown = () => {
+     mouseDown = true;
+   };
+   document.body.onmouseup = () => {
+     mouseDown = false;
+   };
+   function populateBoard(size) {
+     let board = document.querySelector(".board");
+     let squares = board.querySelectorAll("pixels");
+     squares.forEach((pixels) => pixels.remove());
 
- //start colorPicker
+     board.style.display = "grid";
+     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+     amount = size * size;
+     for (i = 0; i < amount; i++) {
+       let square = document.createElement("pixels");
+       square.style.backgroundColor = "white";
+       square.style.border = "0.2px solid black";
+       square.addEventListener("mouseover", squareEvent);
+       square.addEventListener("mousedown", squareEvent);
+       board.appendChild(square);
+     }
+     if (border == true) {
+       borderOn();
+     } else {
+       borderOff();
+     }
+     let sizeInput = document.querySelector(".size");
+     sizeInput.textContent = `${size} x ${size}`;
+   }
 
- function colourPickerOn() {
-   colourPicker.style.opacity = 1;
-   colourPicker.disabled = false;
- }
+   function squareEvent(e) {
+     if (e.type === "mouseover" && !mouseDown) return;
+     if (colour == "random") {
+       randomColour = Math.floor(Math.random() * 16777215).toString(16);
+       this.style.backgroundColor = "#" + randomColour;
+     } else {
+       this.style.backgroundColor = colour;
+     }
+   }
 
- function colourPickerOff() {
-   colourPicker.style.opacity = 0.5;
-   colourPicker.disabled = true;
- }
- //end colourpicker
- function changeColour(markerColour) {
-   colour = markerColour;
-   console.log(markerColour);
- }
+   //start randomButtom
+   function toggleRandom() {
+     if (!random) {
+       randomOn();
+       colourPickerOff();
+       erasorOff();
+     } else {
+       randomOff();
+       colourPickerOn();
+       //colour changes to current colourpicker colour
+       colour = colourPicker.value;
+     }
+   }
+   function randomOn() {
+     randomButton.style.backgroundColor = "grey";
+     randomButton.style.transform = "translate(2px, 2px)";
+     random = true;
+   }
 
- function toggleBorder() {
-   pixels = document.querySelectorAll("pixels");
-   toggleButton = document.getElementById("toggleBorder");
+   function randomOff() {
+     randomButton.style.backgroundColor = "lightgray";
+     randomButton.style.transform = "none";
+     random = false;
+   }
+   // end of randomButton
 
-   if (border == true) {
-     pixels.forEach((pixels) => {
-       pixels.style.border = "white";
-     });
-     toggleButton.textContent = "Add Grid";
-     border = false;
-   } else {
+   //start erasorButton()
+   function toggleErasor() {
+     if (!erasor) {
+       erasorOn();
+       randomOff();
+       colourPickerOff();
+     } else {
+       erasorOff();
+       colourPickerOn();
+       colour = colourPicker.value;
+     }
+   }
+
+   function erasorOn() {
+     erasorButton.style.backgroundColor = "grey";
+     erasorButton.style.transform = "translate(2px, 2px)";
+     erasor = true;
+   }
+
+   function erasorOff() {
+     erasorButton.style.backgroundColor = "lightgray";
+     erasorButton.style.transform = "none";
+     erasor = false;
+   }
+   //end erasorButton
+
+   //start colorPicker
+
+   function colourPickerOn() {
+     colourPicker.style.opacity = 1;
+     colourPicker.disabled = false;
+   }
+
+   function colourPickerOff() {
+     colourPicker.style.opacity = 0.5;
+     colourPicker.disabled = true;
+   }
+   //end colourpicker
+   function changeColour(markerColour) {
+     colour = markerColour;
+     console.log(markerColour);
+   }
+
+   function toggleBorder() {
+     if (border == true) {
+       borderOff();
+       border = false;
+     } else {
+       borderOn();
+       border = true;
+     }
+   }
+   function borderOn() {
+     pixels = document.querySelectorAll("pixels");
+     toggleButton = document.getElementById("toggleBorder");
      pixels.forEach((pixels) => {
        pixels.style.border = "0.2px solid black";
      });
      toggleButton.textContent = "Remove Grid";
-     border = true;
    }
- }
 
+   function borderOff() {
+     pixels = document.querySelectorAll("pixels");
+     toggleButton = document.getElementById("toggleBorder");
+     pixels.forEach((pixels) => {
+       pixels.style.border = "white";
+     });
+     toggleButton.textContent = "Add Grid";
+   }
  //Changed toggle to mousedown and mousehover(press and drag)
 //  click = false;
 //  document.querySelector("body").addEventListener("click", (e) => {
